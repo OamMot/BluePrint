9ec118c13c5f428d8d717264acb1d8bd27e0d7be
gaomingcheng <gaomingcheng@baidu.com>
2017-03-09 14:43:10 +0800

    update

--- LogModule/logInit.py
+++ LogModule/logInit.py
@@ -3,15 +3,16 @@ import logging
 import time
 import os
 
-YMDH = time.strftime('%Y-%m-%d-%H',time.localtime(time.time()))
-dic = '../logs'
-if (os.path.exists(dic) == False):
-    os.makedirs(dic)
+def initLog():
+    YMDH = time.strftime('%Y-%m-%d-%H',time.localtime(time.time()))
+    dic = '../logs'
+    if (os.path.exists(dic) == False):
+        os.makedirs(dic)
 
-filename = '../logs/' + YMDH + '.log'
+    filename = '../logs/' + YMDH + '.log'
 
-logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s %(funcName)s',
-                datefmt='%a, %d %b %Y %H:%M:%S',
-                filename=filename,
-                filemode='w')
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s %(funcName)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename=filename,
+                    filemode='w')
