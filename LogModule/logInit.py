f060123ff57e7240b2a96e0da43d5120e9dba975
gaomingcheng <gaomingcheng@baidu.com>
2017-03-09 14:39:00 +0800

    日志模块完善

--- LogModule/logInit.py
+++ LogModule/logInit.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
+import logging
+import time
+import os
 
+YMDH = time.strftime('%Y-%m-%d-%H',time.localtime(time.time()))
+dic = '../logs'
+if (os.path.exists(dic) == False):
+    os.makedirs(dic)
+
+filename = '../logs/' + YMDH + '.log'
+
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s %(funcName)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename=filename,
+                filemode='w')
