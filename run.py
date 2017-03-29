87104fe1cb483f82df975dbbde6f70fd019b3a58
gaomingcheng <gaomingcheng@baidu.com>
2017-03-29 19:55:47 +0800

    配置

--- run.py
+++ run.py
@@ -6,16 +6,16 @@ from User import user
 from LogModule.log import *
 import time
 
-apple = Flask(__name__)
+application = Flask(__name__)
 
 # blueprint router register
-apple.register_blueprint(zhihu, url_prefix='/zhihu')  # 注册asset蓝图，并指定前缀。
-apple.register_blueprint(jianshu, url_prefix='/jianshu')  # 注册asset蓝图，并指定前缀。
-apple.register_blueprint(user, url_prefix='/user')  # 注册asset蓝图，并指定前缀。
+application.register_blueprint(zhihu, url_prefix='/zhihu')  # 注册asset蓝图，并指定前缀。
+application.register_blueprint(jianshu, url_prefix='/jianshu')  # 注册asset蓝图，并指定前缀。
+application.register_blueprint(user, url_prefix='/user')  # 注册asset蓝图，并指定前缀。
 
 # init_log('./log/app')
 YMDH = time.strftime('%Y-%m-%d-%H',time.localtime(time.time()))
-init_log('./logs/' + YMDH + '.log', apple.logger)
+init_log('./logs/' + YMDH + '.log', application.logger)
 
 if __name__ == '__main__':
-    apple.run(host='0.0.0.0', port=5000)  # 运行flask http程序，host指定监听IP，port指定监听端口，调试时需要开启debug模式。
+    application.run(host='0.0.0.0', port=5000)  # 运行flask http程序，host指定监听IP，port指定监听端口，调试时需要开启debug模式。
